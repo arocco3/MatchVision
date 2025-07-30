cfc4ee88b8d3d68c7bd6d0a5a1efc276f3f4a8a3
alespix <a.rocco3@studenti.unibg.it>
2025-07-30 19:04:57 +0200

    Aggiunti components
    Creati footer e header

--- MatchVision/src/app/app.ts
+++ MatchVision/src/app/app.ts
@@ -5,11 +5,19 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { MatchComponent } from './pages/match/match.component';
 import { StatisticsComponent } from './pages/statistics/statistics.component';
 import { LoginComponent } from "./pages/login/login.component";
+import { FooterComponent } from "./pages/footer/footer.component";
+import { HeaderComponent } from "./pages/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, DashboardComponent],
+  imports: [
+    RouterOutlet,
+    LoginComponent,
+    DashboardComponent,
+    FooterComponent,
+    HeaderComponent
+],
   templateUrl: './app.html',
   styleUrls: ['./app.scss', './pages/login/login.component.scss', './pages/dashboard/dashboard.component.scss']
 })
