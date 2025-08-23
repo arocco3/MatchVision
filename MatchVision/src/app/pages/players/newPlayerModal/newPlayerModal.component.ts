3606e97a3d75b39f46a9b9df63aa2add77a208a8
alespix <a.rocco3@studenti.unibg.it>
2025-08-23 18:22:37 +0200

    Ora si possono aggiungere players e visualizzarli

--- MatchVision/src/app/pages/players/newPlayerModal/newPlayerModal.component.ts
+++ MatchVision/src/app/pages/players/newPlayerModal/newPlayerModal.component.ts
@@ -1,23 +1,34 @@
-import { PlayersService } from '../../../services/players.service'
+import { Player, PlayersService } from '../../../services/players.service'
 
 import { Component, inject, signal, TemplateRef, ViewChild, WritableSignal } from '@angular/core';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms'
 
 @Component({
 	selector: 'app-new-player-modal',
   standalone: true,
+  imports: [FormsModule],
 	templateUrl: './newPlayerModal.component.html',
   styleUrls: ['./newPlayerModal.component.scss']
 })
 
 export class NewPlayerModalComponent {
-
+  
+  private playersService = inject(PlayersService);
   private modalService = inject(NgbModal);
   public closeResult: WritableSignal<string> = signal('');
 
+newPlayer: Player = {
+  name: '',
+  surname: '',
+  number: 0,
+  role: ''
+};
+
   @ViewChild('content', { static: true }) content!: TemplateRef<any>;
 
- public open() {
+  // To open the modal
+  public open() {
     this.modalService.open(this.content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
       (result) => {
         this.closeResult.set(`Closed with: ${result}`);
@@ -26,6 +37,21 @@ export class NewPlayerModalComponent {
         // this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
       },
     );
+  }
+
+  // To save the player
+  public savePlayer(form: any, modal: any) {
+    if (form.valid) {
+      this.playersService.createPlayer(this.newPlayer).subscribe({
+        next: (res) => {
+          console.log('Giocatore salvato:', res);
+          modal.close('Save click');
+          // Reset form
+          this.newPlayer = { name: '', surname: '', number: 0, role: '' };
+        },
+        error: (err) => console.error('Errore salvataggio nuovo player', err)
+      });
+    }
   }
      
 }
--- backend/MatchVisionApp/urls.py
+++ backend/MatchVisionApp/urls.py
@@ -20,33 +20,33 @@ urlpatterns = [
     # Players
     path('players/', views.getPlayers),
     path('players/<int:pk>', views.getPlayer),
-    path('players/create', views.createPlayer),
+    path('players/create/', views.createPlayer),
     path('players/update/<int:pk>', views.updatePlayer),
     path('players/delete/<int:pk>', views.deletePlayer),
 
     # Teams
     path('teams/', views.getTeams),
     path('teams/<int:pk>', views.getTeam),
-    path('teams/create', views.createTeam),
+    path('teams/create/', views.createTeam),
     path('teams/delete/<int:pk>', views.deleteTeam),
 
     # Matches
     path('matches/', views.getMatches),
     path('matches/<int:pk>', views.getMatch),
-    path('matches/create', views.createMatch),
+    path('matches/create/', views.createMatch),
     path('matches/delete/<int:pk>', views.deleteMatch),
 
     # Sets
     path('sets/', views.getSets),
-    path('sets/create', views.createSet),
+    path('sets/create/', views.createSet),
     path('sets/delete/<int:pk>', views.deleteSet),
 
     # Events
     # path('events', views.getEvents),
-    path('events/create', views.createEvent),
+    path('events/create/', views.createEvent),
 
     # Touches
     path('touches/', views.getTouches),
-    path('touches/create', views.createTouch),
+    path('touches/create/', views.createTouch),
 
 ]
