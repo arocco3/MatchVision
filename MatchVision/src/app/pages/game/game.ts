ef17deeeb9d54bbed60f591057c34905bd15afc9
alespix <a.rocco3@studenti.unibg.it>
2025-08-26 21:48:09 +0200

    Implementate funzioni pagina game

--- MatchVision/src/app/pages/game/game.ts
+++ MatchVision/src/app/pages/game/game.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Player } from '../../services/players.service';
 
 @Component({
     selector: 'app-game',
@@ -13,20 +14,46 @@ import { RouterModule } from '@angular/router';
 
 export class GameComponent{
 
-    // Coordinates [x%, y%]
-    pos_1: [number, number] = [33, 75];
-    pos_2: [number, number] = [80, 75];
-    pos_3: [number, number] = [80, 50];
-    pos_4: [number, number] = [80, 25];
-    pos_5: [number, number] = [33, 25];
-    pos_6: [number, number] = [33, 50];
-    libero: [number, number] = [99, 99];
+        // Coordinates [x%, y%]
+    pos: [number, number][] = [
+        [15, 80], // pos 1
+        [42, 80], // pos 2
+        [42, 50], // pos 3
+        [42, 20], // pos 4
+        [15, 20], // pos 5
+        [15, 50], // pos 6
+    ];
+
+    libero_pos: [number, number] = [0, 99] // Libero
 
     score = {home: 0, guests: 0};
     selectedPlayer: any = null;
     selectedFundamental: string = '';
     selectedOutcome: string = '';
-    players: any;
+    //players: Player[] = [];
+
+    // players di prova sarbbero i titolari
+    players: Player[] = [
+        { id: 1, name: "Luca",   surname: "Rossi",   number: 1, role: "Setter"},
+        { id: 2, name: "Marco",  surname: "Bianchi", number: 2, role: "Opposto" },
+        { id: 3, name: "Gianni", surname: "Verdi",   number: 3, role: "Centrale"},
+        { id: 4, name: "Paolo",  surname: "Neri",    number: 4, role: "Schiacciatore"},
+        { id: 5, name: "Andrea", surname: "Russo",   number: 5, role: "Centrale"},
+        { id: 6, name: "Matteo", surname: "Ferrari", number: 6, role: "Schiacciatore"}
+    ];
+
+    libero: Player = { id: 7, name: "Simone", surname: "Galli",  number: 7, role: "Libero"};
+
+    // panchinari
+    players_in_panchina: Player[] = [
+        { id: 8, name: "1luca1",   surname: "Rossi",   number: 8, role: "Setter"},
+        { id: 9, name: "Marco2",  surname: "Bianchi", number: 9, role: "Opposto" },
+        { id: 10, name: "Gianni3r4", surname: "Verdi",   number: 10, role: "Centrale"},
+        { id: 11, name: "Paolo34",  surname: "Neri",    number: 11, role: "Schiacciatore"},
+        { id: 12, name: "Andrea34t3", surname: "Russo",   number: 12, role: "Centrale"},
+        { id: 13, name: "Matteo3t3", surname: "Ferrari", number: 13, role: "Schiacciatore"}
+    ];
+
     
     increaseScore(team: 'home' | 'guests') {
         this.score[team]++;
@@ -42,8 +69,21 @@ export class GameComponent{
         throw new Error('Method not implemented.');
     }
 
-    insertTouch(arg0: any) {
+    insertTouch(p: Player) {
+        console.log("Premuto su insertouch di", p.id, p.name );
+    }
 
+    // Rotation of players
+    do_rotation(): void {
+        const last = this.pos.pop();
+        if (last) {
+            this.pos.unshift(last);
+        }
+    }
+
+    //change player
+    change_player(): void {
+        //apre modale e scambia i giocatori tra le due liste
     }
 
     endSet() {
